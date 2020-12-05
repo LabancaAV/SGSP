77f1d6ab4c71dd72f262f42b5d9b9be01c98988f
André Labanca <andrelabanca2@gmail.com>
2020-12-04 21:54:54 -0300

    Starting making inputs in the CreateMedico file.

--- web/src/pages/CreateMedico/index.tsx
+++ web/src/pages/CreateMedico/index.tsx
@@ -1,8 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import "./styles.css";
+
+import logo from "../../assets/logo.svg";
 
 const CreateMedico = () =>{
   return(
-    <h1>Hello Planet.</h1>
+
+    <div id="page-create-medico">
+
+      <header>
+
+        <img src={logo} alt="SGSP logo"/>
+
+        <Link to="/">
+          <FiArrowLeft />
+          voltar para Home
+        </Link>
+
+      </header>
+
+      <form>
+
+        <h1>Cadastro <br/> do Médico</h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome do médico</label>
+            <input 
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+          </legend>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Especialidades</h2>
+          </legend>
+        </fieldset>
+
+      </form>
+
+    </div>
   );
 }
 
